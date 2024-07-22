0e0edfd3bdcc4ade5f24b4245218f960347bb4ce
Alexey <pancho156932@gmail.com>
2024-07-22 17:54:42 +0300

    Добавлен список ID подзадач

--- src/com/yandex/taskTracker/model/Epic.java
+++ src/com/yandex/taskTracker/model/Epic.java
@@ -1,9 +1,24 @@
 package com.yandex.taskTracker.model;
 
+import java.util.ArrayList;
+
 public class Epic extends Task {
 
+    private final ArrayList<Integer> subTasksId = new ArrayList<>();
     public Epic(String name, String description) {
         super(name,description);
+    }
+    public ArrayList<Integer> getSubTasksList() {
+        return subTasksId;
+    }
+    public void addSubtaskId(Integer id){
+        subTasksId.add(id);
+    }
+    public void removeSubTaskId(Integer id){
+       subTasksId.remove(id);
+    }
+    public void clearSubTaskId(){
+        subTasksId.clear();
     }
     @Override
     public String toString() {
@@ -12,6 +27,7 @@ public class Epic extends Task {
                 ", description='" + getDescription() + '\'' +
                 ", status=" + getStatus() +
                 ", id=" + getId() +
+                ", subTasksList.size()=" + subTasksId.size() +
                 '}';
     }
 }
