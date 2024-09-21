964fa268e44cb4743f874d5199fdb8dbac660bb3
Алексей Панченко <alyosha.panchenko@yandex.ru>
2024-09-21 21:41:55 +0300

    добавлен статичный метод loadFromFile

--- src/com/yandex/taskTracker/service/Managers.java
+++ src/com/yandex/taskTracker/service/Managers.java
@@ -2,13 +2,21 @@ package com.yandex.taskTracker.service;
 
 import com.yandex.taskTracker.service.HistoryManager.HistoryManager;
 import com.yandex.taskTracker.service.HistoryManager.InMemoryHistoryManager;
+import com.yandex.taskTracker.service.TaskManager.FileBackedTaskManager;
 import com.yandex.taskTracker.service.TaskManager.InMemoryTaskManager;
 import com.yandex.taskTracker.service.TaskManager.TaskManager;
+
+import java.io.File;
 
 public class Managers {
 
     public static TaskManager getDefault() {
         return (TaskManager) new InMemoryTaskManager();
+    }
+
+    public static FileBackedTaskManager loadFromFile(File file){
+        FileBackedTaskManager.saveFile = file.toPath();
+        return new FileBackedTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
