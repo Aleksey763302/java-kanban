39fe1cf1f6ed6f02203e9f64f5dc58b79fd8fc45
Алексей Панченко <alyosha.panchenko@yandex.ru>
2024-09-23 12:36:03 +0300

    изменен конструктор

--- src/com/yandex/taskTracker/exceptions/ManagerSaveException.java
+++ src/com/yandex/taskTracker/exceptions/ManagerSaveException.java
@@ -3,7 +3,7 @@ package com.yandex.taskTracker.exceptions;
 import java.io.IOException;
 
 public class ManagerSaveException extends IOException {
-    public ManagerSaveException(String message, Throwable throwable) {
-        super(message, throwable);
+    public ManagerSaveException(String str) {
+        super(str);
     }
 }
